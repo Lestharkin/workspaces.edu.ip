b84991a4dbf5781db258acc56bd8d01f104c15c8
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-08-11 08:36:42 -0500

    sync

---
+++ tdd/src/main/java/pi/test/Factorial.java
@@ -0,0 +1,5 @@
+package pi.test;
+
+public class Factorial {
+  
+}
---
+++ tdd/src/test/java/pi/test/FactorialTest.java
@@ -0,0 +1,12 @@
+package pi.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FactorialTest {
+
+  @Test
+  @DisplayName("")
+
+  
+}
